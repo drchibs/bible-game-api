f0c789c8f7c99a4a4186fae4f1999109bc010656
drchibs <richarddennis789@gmail.com>
2022-05-13 08:36:11 +0100

    Feat:(Security) Helmet, CORS, Rate Limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,23 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DBModule } from './db/db.module';
 import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from './roles.guard';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [UserModule, AuthModule, DBModule],
-  /*providers: [
+  imports: [
+    UserModule,
+    AuthModule,
+    DBModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+  ],
+  providers: [
     {
       provide: APP_GUARD,
-      useClass: RolesGuard,
+      useClass: ThrottlerGuard,
     },
-  ],*/
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DBService } from './db/db.service';
-import { RolesGuard } from './roles.guard';
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //app.useGlobalGuards(new RolesGuard());
+  app.use(helmet());
+  app.enableCors();
   const dbService: DBService = app.get(DBService);
   await dbService.enableShutdownHooks(app);
   await app.listen(3000);
