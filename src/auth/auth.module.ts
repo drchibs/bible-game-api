c2aec8f365e65f749d69c875861915a0a5a9a8c4
drchibs <richarddennis789@gmail.com>
2022-05-13 05:36:29 +0100

    Feat: local strategy login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { DBModule } from 'src/db/db.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UserModule, DBModule],
-  providers: [AuthService],
+  imports: [UserModule, DBModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
