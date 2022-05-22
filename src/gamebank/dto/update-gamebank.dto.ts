a395014803d248f7e2dd303b28f696a05c352732
drchibs <richarddennis789@gmail.com>
2022-05-22 12:54:41 +0100

    feat: game rank seed, difficulties seed, gamebank controllers

--- src/gamebank/dto/update-gamebank.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateGamebankDto } from './create-gamebank.dto';
-
-export class UpdateGamebankDto extends PartialType(CreateGamebankDto) {}
